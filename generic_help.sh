295f5a47bd4c6647522f228849a40da2bee000f0
Bruce Segee <segee@maine.edu>
2020-12-30 13:05:23 -0500

    Major update, now can use local (fixed) ip or global (floating) ip in addition to server name

--- generic_help.sh
+++ generic_help.sh
@@ -1,7 +1,7 @@
         echo 'To use this script, please first install the'
         echo 'python openstack client.'
         echo 'In ubuntu this can be done with:'
-        echo 'sudo apt-get install python-openstackclient'
+        echo 'sudo apt-get install python3-openstackclient'
         echo 
         echo 'Next, the openstack rc file must be downloaded'
         echo 'this can be done by logging into the openstack'
@@ -11,7 +11,7 @@
         echo 'copy the file to the directory containing this '
         echo 'script'
         echo
-        echo 'Finally, create a text file named mypassword'
+        echo 'Finally, create a text file named myi_password'
         echo 'that contains your openstack password.  Use'
         echo 'chmod 000 mypassword'
         echo 'so the file is not readable by anyone.'
--- openstack_unshelve_machine.sh
+++ openstack_unshelve_machine.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
+unset NAME
+unset GLOBAL
+unset LOCAL
+
 if [ $# -eq 0 ]
 then
         source generic_help.sh
         echo
-        echo "usage: $0 name_of_machine_to_stop"
+        echo "usage: $0 name_of_machine_to_unshelve"
+	echo
+        echo "or"
         echo
+        echo "$0 -l local.ip.of.machine"
+        echo "e.g., $0 -l 192.168.0.2"
+        echo "(local ip is the same as fixed ip in openstack)"
+        echo 
+        echo "or"
+        echo 
+        echo "$0 -g global.ip.of.machine"
+        echo "e.g., $0 -g 204.197.12.34"
+        echo "(global ip is the same as floating ip in openstack)"
+        echo "combining options is generally not helpful"
+
+	echo
         exit 1
 fi
 
+
+if [ $# -eq 1 ]
+then
+        NAME=$1
+else
+        while getopts 'l:g:' OPTION; do
+                case $OPTION in
+                        l)
+                                LOCAL=$OPTARG
+                                ;;
+                        g)
+                                GLOBAL=$OPTARG
+                esac
+        done
+fi
 
 
 # Needs a file called my_password in the local directory
@@ -15,5 +48,15 @@ fi
 chmod 400 my_password
 source *openrc.sh < my_password
 chmod 000 my_password
-openstack server unshelve $1
+if [[ -v GLOBAL ]]
+then
+LOCAL=`/usr/bin/openstack floating ip list -f value --floating-ip-address "$GLOBAL" -c 'Fixed IP Address' `
+fi
+
+if [[ -v LOCAL ]]
+then
+NAME=`/usr/bin/openstack server list -c Name -f value --ip "$LOCAL" `
+fi
+
+openstack server unshelve $NAME
 
